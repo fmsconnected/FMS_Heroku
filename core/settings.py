09c5a65aab88219e0db11cc5993ad9f3fef4bc0d
MJMarkjohn <mjmarkjohn007@gmail.com>
2020-03-09 15:19:09 +0800

    djangoheroku

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os
+import 
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -157,4 +158,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework_datatables.pagination.DatatablesPageNumberPagination',
     'PAGE_SIZE': 50,
 }
-
+django_heroku.settings(locals())
