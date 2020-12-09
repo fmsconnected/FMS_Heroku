8b1f8c8de3d1a7f9eb72af7e2f18b2be91bf4ef1
MJMarkjohn <mjmarkjohn007@gmail.com>
2020-12-09 14:55:43 +0800

    --- Auto-Git Commit ---

--- CustomerLog/views.py
+++ CustomerLog/views.py
@@ -7,6 +7,13 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from django.shortcuts import render
 from bootstrap_modal_forms.generic import BSModalDeleteView
+# User
+from django.core.exceptions import PermissionDenied
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.views.generic import (
     ListView,
@@ -28,7 +35,17 @@ from .models import (
 )
 
 
+def in_group(user):
+    if user.groups.filter(name="CustomerLog").exists():
+        return True
+    else:
+        raise PermissionDenied
+
+
 class CSCreateView(CreateView):
+    # @method_decorator(user_passes_test(in_group))
+    # def dispatch(self, *args, **kwargs):
+    #     return super().dispatch(*args, **kwargs)
     model = CS_log
     form_class = CS_form
     template_name = 'CS/CS_create.html'
@@ -45,30 +62,43 @@ class CSCreateView(CreateView):
 
 
 class CSListView(ListView):
+    # @method_decorator(user_passes_test(in_group))
+    # def dispatch(self, *args, **kwargs):
+    #     return super().dispatch(*args, **kwargs)
     model = CS_log
     template_name = 'CS/CS_list.html'
 
 
 class CSDetails(DetailView):
+    # @method_decorator(user_passes_test(in_group))
+    # def dispatch(self, *args, **kwargs):
+    #     return super().dispatch(*args, **kwargs)
     model = CS_log
     template_name = 'CS/CS_details.html'
 
 
 class CSUpdate(UpdateView):
+    # @method_decorator(user_passes_test(in_group))
+    # def dispatch(self, *args, **kwargs):
+    #     return super().dispatch(*args, **kwargs)
     model = CS_log
     form_class = CS_formupdate
     template_name = 'CS/CS_update.html'
 
 
 class CSDeleteView(BSModalDeleteView):
+    @method_decorator(user_passes_test(in_group))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
     model = CS_log
     template_name = 'CS/CS_delete.html'
     success_message = 'Success: Item was deleted.'
     success_url = reverse_lazy('CS_List')
 
 
+# @user_passes_test(in_group)
 def customer_log_excel(request):
-    customer_queryset = CS_log.objects.all()   
+    customer_queryset = CS_log.objects.all()
     response = HttpResponse(
         content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
     )
@@ -79,17 +109,17 @@ def customer_log_excel(request):
     worksheet.title = 'Customer Care Log'
 
     columns = [
-            'Activity Id',
-            'Date Received',
-            'Fleet Member',
-            'Client Name',
-            'Email',
-            'Mobile Number',
-            'Transaction Type',
-            'Plate Number',
-            'Problem',
-            'Date Resolved',
-            'Action Taken'
+        'Activity Id',
+        'Date Received',
+        'Fleet Member',
+        'Client Name',
+        'Email',
+        'Mobile Number',
+        'Transaction Type',
+        'Plate Number',
+        'Problem',
+        'Date Resolved',
+        'Action Taken'
     ]
     row_num = 1
 
@@ -100,19 +130,19 @@ def customer_log_excel(request):
     for cc in customer_queryset:
         row_num += 1
         row = [
-                cc.Activity_id,
-                cc.Date_received,
-                cc.Fleet_member,
-                cc.Client_name,
-                cc.Email,
-                cc.Mobile_no,
-                cc.Transaction_type,
-                cc.Plate_no,
-                cc.Problem,
-                cc.Date_resolved,
-                cc.Action_taken
+            cc.Activity_id,
+            cc.Date_received,
+            cc.Fleet_member,
+            cc.Client_name,
+            cc.Email,
+            cc.Mobile_no,
+            cc.Transaction_type,
+            cc.Plate_no,
+            cc.Problem,
+            cc.Date_resolved,
+            cc.Action_taken
         ]
-        
+
         for col_num, cell_value in enumerate(row, 1):
             cell = worksheet.cell(row=row_num, column=col_num)
             cell.value = cell_value
