9d29170746a05e4dd1c759db1bd7cf5f95b2ea23
MJMarkjohn <mjmarkjohn007@gmail.com>
2021-10-15 15:48:23 +0800

    fsp_status_v1

--- core/urls.py
+++ core/urls.py
@@ -64,7 +64,7 @@ urlpatterns = [
     path('Monthly/Report/Shell/', include('monthly_report_shell.urls')),
     path('Battery/', include('battery.urls')),
     path('Tire/', include('tires.urls')),
-    path('Fuel-Supplier/', include('fuel_supplier_payment.urls')),
+    # path('Fuel-Supplier/', include('fuel_supplier_payment.urls')),
     path('', auth_views.LoginView.as_view(
         template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='logout'),
