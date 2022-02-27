26b7fa81f356814943c023dbed565acb52f70886
MJMarkjohn <mjmarkjohn007@gmail.com>
2022-02-27 19:55:07 +0800

    core_error

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_datatables',
     'ajax_select',
-    'bootstrap3',
     'bootstrap_modal_forms',
     'widget_tweaks',
     'simple_history',
